996835439f75d84b848fcfc1a0a42b5b8fbecfdd
hyun5085 <ksw9250@naver.com>
2025-04-16 18:40:24 +0900

    Level 1-3: Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,7 @@ public class UserChangePasswordRequest {
     @NotBlank
     private String oldPassword;
     @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).{8,}$",
+            message = "비밀번호는 대문자, 숫자를 포함해야 합니다.")
     private String newPassword;
 }
