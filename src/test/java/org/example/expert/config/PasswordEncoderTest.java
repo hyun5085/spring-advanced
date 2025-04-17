2fd6bedbba3a626cf59800af39d79d60b67ac539
hyun5085 <ksw9250@naver.com>
2025-04-17 22:17:40 +0900

    Level 3-1: 패스워드 암호화 테스트코드 !

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -19,8 +19,8 @@ class PasswordEncoderTest {
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // when  ( 순서 바뀜! )
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
         assertTrue(matches);
