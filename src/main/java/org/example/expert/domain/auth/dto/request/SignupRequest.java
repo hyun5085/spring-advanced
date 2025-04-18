67e11cc1a707dad77d2870cfc38da002a0e0aeb9
hyun5085 <ksw9250@naver.com>
2025-04-19 00:02:12 +0900

    레벨 5: ‘내’가 정의한 문제와 해결 과정

--- src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,7 @@ package org.example.expert.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +14,12 @@ public class SignupRequest {
 
     @NotBlank @Email
     private String email;
+
     @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).{8,}$",
+            message = "비밀번호는 8글자 이상, 대문자, 숫자를 포함해야 합니다.")
     private String password;
+
     @NotBlank
     private String userRole;
 }
